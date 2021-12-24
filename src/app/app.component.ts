1c18e93e3b00e052ce04d8ab7312f13de9940ce0
Cefjulio <juliorecinos30@gmail.com>
2021-12-24 15:01:37 -0600

    V.16 Creacion de un component en Angular con CLI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'mi-primera-app';
-  saludo = 'Saludos desde Angular';
+  titulo = 'mi-primera-app';
+  
 }
