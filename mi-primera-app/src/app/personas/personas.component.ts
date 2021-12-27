47703977fd9ce0b2287ee713908f16d3badeafb4
Julio Cefnar Auyon R <juliorecinos30@gmail.com>
2021-12-27 06:48:30 +0000

    ngIf

--- mi-primera-app/src/app/personas/personas.component.ts
+++ mi-primera-app/src/app/personas/personas.component.ts
@@ -9,10 +9,12 @@ import { Component } from "@angular/core";
 export class PersonasComponent{
     deshabilitar = false;
     titulo = "ingeniero";
+    mostrar=false;
 
     mensaje= 'No se ha agregado ninguna persona';
 
     agregarPersona(){
+        this.mostrar = true;
         this.mensaje = 'Persona agregada';
     }
 
